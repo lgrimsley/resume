9920c1e231ccc51552e07c747e89ce56b3ff5e66
= <=>
2023-12-06 14:41:15 -0500

    Added Google Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import './index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+if (import.meta.env.VITE_GA_ID && import.meta.env.VITE_GA_ID !== '') {   
+    app.use(VueGtag, {
+        appName: "Resume",
+        pageTrackerScreenviewEnabled: true,
+        config: { id: import.meta.env.VITE_GA_ID }
+    })
+}
 app.mount('#app')
