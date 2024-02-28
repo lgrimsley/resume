122bd806fa09de4fac7cc53364f9952c8e9447e6
gertnerbot <gertnerbot@gmail.com>
2024-02-28 17:32:56 +0000

    fixed education

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,85 +1,51 @@
 <script setup lang="ts">
+import { useAppStore } from '@/stores/app.store';
+import { type Education } from '@/types/resume';
+import { Icon } from '@iconify/vue/dist/iconify.js';
+import { computed, type PropType } from 'vue';
+import SkillSmall from './SkillSmall.vue';
 
+const props = defineProps({
+    data: {
+        type: Array as PropType<Education[]>
+    }
+});
+
+const formatDate = (dateString: string) => {
+    if (!dateString) return '';
+
+    const [year, month] = dateString.split('-');
+    const date = new Date(Number(year), Number(month) - 1);
+    return date.toLocaleString('default', { month: 'short', year: 'numeric' }); // e.g., 'May 2022'
+};
+
+const borderClass = computed(() => useAppStore().getBorderClass);
+const textColorClass = computed(() => useAppStore().getTextColorClass);
 </script>
 
 <template>
-    <div class="p-7 shadow md:rounded-xl bg-white overflow-hidden relative dark:bg-zinc-700 dark:text-white"><!-- Start Education Block -->
-                    <h2 class="text-lg font-semibold mb-4">Education</h2>
-                    <div class="mb-5 item-section">
-                        <div class="flex-shrink-0 w-12 h-12 rounded-xl bg-cover" style="background-image: url('assets/img/bu-logo.jpg');">
-                        </div>
-
-                        <div class="w-full space-y-5">
-                            <div class="item-header items-end">
-                                <div class="space-y-1.5">
-                                    <div class="font-medium">Master in Software Engineering</div>
-                                    <div class="flex space-x-5">
-                                        <div class="item-header-info">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                                            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
-                                            </svg>
-                                            <span>Brunel University</span>
-                                        </div>
-                                        <div class="item-header-info">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                            </svg>
-                                            <span>London</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="space-y-1.5 sm:text-right">
-                                    <div class="item-header-info">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-                                        </svg>
-                                        <span>September 2017 - May 2019</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="border-b border-gray-200"></div>
-                        </div>
-                    </div>
-
-                    <div class="item-section">
-                        <div class="flex-shrink-0 w-12 h-12 rounded-xl bg-cover" style="background-image: url('assets/img/bu-logo.jpg');">
-                        </div>
-
-                        <div class="w-full space-y-5">
-                            <div class="item-header items-end">
-                                <div class="space-y-1.5">
-                                    <div class="font-medium">Master in Software Engineering</div>
-                                    <div class="flex space-x-5">
-                                        <div class="item-header-info">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                                            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
-                                            </svg>
-                                            <span>Brunel University</span>
-                                        </div>
-                                        <div class="item-header-info">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                            </svg>
-                                            <span>London</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="space-y-1.5 sm:text-right">
-                                    <div class="item-header-info">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-                                        </svg>
-                                        <span>September 2013 - May 2017</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- Start Education Block -->
+    <div class="overflow-hidden relative dark:text-white flex flex-col gap-8">
+        <div class="bg-white/80 dark:bg-zinc-800/20 p-6 md:p-8 rounded-sm" v-for="education in data">
+            <div class="flex-col gap-2 flex-grow">
+                <a :href="education.url" target="_blank" >
+                    <h3 class="text-3xl font-semibold uppercase tracking-wider flex">
+                        {{ education?.institution }} 
+                        
+                    </h3>
+                </a>
+                <ul>
+                    <li>{{ education.area }}
+                    </li>
+                </ul>
+                <div class="flex flex-col flex-2">
+                        <Icon icon="majesticons:chevron-right" width="20px" :class="`absolute -left-6 top-[3px] ${textColorClass}`" />
+                        {{ education.studyType }} 
+                        <small class="-ml-1 mt-1 text-zinc-600 dark:text-gray-400 font-normal flex items-center gap-1 text-sm ">
+                            <Icon icon="ic:baseline-turn-left" width="22px" class="rotate-180 -mt-1"  />
+                            {{ formatDate(education.startDate) }} - {{ formatDate(education.endDate) }} 
+                        </small>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
